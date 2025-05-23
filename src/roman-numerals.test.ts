8be462ce586ee4502e7e40087861e6d7b4b46c1c
Piras Mattéo <matteo.piras1@etu.univ-lorraine.fr>
2025-05-23 11:20:41 +0200

    REFACTORING123

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -2,12 +2,17 @@ import { it, expect, test } from "vitest"; //importe les méthodes de tests néc
 
 class RomanNumeral {
     correspondance(value: string): number {
-        if (value === "I") return 1
-        if (value === "II") return 2
-        if (value === "III") return 3
+        let compteur = 0
+        for (let c of value) {
+            if (c === "I") compteur++
+        }
+
+        // if (value === "I") return 1
+        // if (value === "II") return 2
+        // if (value === "III") return 3
         if (value === "IV") return 4
         if (value === "V") return 5
-        return 0
+        return compteur
     }
 }
 
@@ -19,7 +24,6 @@ it("Should return as much as there are 'I' ", function () {
     expect(chiffre.correspondance("I")).toBe(1);
     expect(chiffre.correspondance("II")).toBe(2);
     expect(chiffre.correspondance("III")).toBe(3);
-    expect(chiffre.correspondance("IV")).toBe(4);
 });
 
 it("Should return 4", function () {
