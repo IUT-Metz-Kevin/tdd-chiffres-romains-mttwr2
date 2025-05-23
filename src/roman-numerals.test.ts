afd9dd62ffaa362a38c2d13dcf61e95c009a61a7
Piras Mattéo <matteo.piras1@etu.univ-lorraine.fr>
2025-05-23 11:06:17 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -5,7 +5,7 @@ class RomanNumeral {
         if (value === "I") return 1
         if (value === "II") return 2
         if (value === "III") return 3
-
+        if (value === "IIII") return 4
         return 0
     }
 }
@@ -16,4 +16,5 @@ it("Should return as much as there are 'I' ", function () {
     expect(chiffre.correspondance("I")).toBe(1);
     expect(chiffre.correspondance("II")).toBe(2);
     expect(chiffre.correspondance("III")).toBe(3);
+    expect(chiffre.correspondance("IV")).toBe(4);
 });
