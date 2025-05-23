20a9842564f84cdb4c82d9896fa270f70c2fac70
Piras Mattéo <matteo.piras1@etu.univ-lorraine.fr>
2025-05-23 11:01:13 +0200

    GREEN2

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -3,6 +3,7 @@ import { it, expect, test } from "vitest"; //importe les méthodes de tests néc
 class RomanNumeral {
     correspondance(value: string): number {
         if (value === "I") return 1
+        if (value === "II") return 2
 
         return 0
     }
@@ -10,6 +11,7 @@ class RomanNumeral {
 
 let chiffre = new RomanNumeral()
 
-it("Should return '1' when value is I", function () {
+it("Should return as much as there are 'I' ", function () {
     expect(chiffre.correspondance("I")).toBe(1);
+    expect(chiffre.correspondance("II")).toBe(2);
 });
