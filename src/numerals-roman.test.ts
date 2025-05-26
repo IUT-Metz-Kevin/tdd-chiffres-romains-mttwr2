ffc40b020e5870ca7cb2bb427c381ce67134e4b2
Piras Mattéo <matteo.piras1@etu.univ-lorraine.fr>
2025-05-26 15:09:16 +0200

    GREEN

--- src/numerals-roman.test.ts
+++ src/numerals-roman.test.ts
@@ -6,6 +6,7 @@ class NumeralRoman {
         if (value === 2) return "II"
         if (value === 3) return "III"
         if (value === 4) return "IV"
+        if (value === 5) return "V"
         return ""
     }
 }
@@ -17,4 +18,5 @@ it("Should return as much I as the value", function () {
     expect(Roman.correspondance(2)).toBe("II");
     expect(Roman.correspondance(3)).toBe("III");
     expect(Roman.correspondance(4)).toBe("IV");
+    expect(Roman.correspondance(5)).toBe("V");
 });
